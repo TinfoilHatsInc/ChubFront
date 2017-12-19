193807d2a4dbf4cca6f0eb0e10adfd5bd35c14b6
mskelo <matejskelo@gmail.com>
2017-12-19 13:13:08 +0100

    HTML form handling v0.0.0.0.1 lol

--- core/login/LoginController.php
+++ core/login/LoginController.php
@@ -2,8 +2,21 @@
 
 namespace core\login;
 
-class LoginController
-{
+$name = $password = $rememberMe = "";
+
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		$name = validateInput($_POST["uname"]);
+		$password = validateInput($_POST["pw"]);
+		$rememberMe = $_POST["checkbox"]);
+	}
+
+	public static function validateInput($data){
+		$data = trim($data);
+		$data = stripslashes($data);
+		$data = htmlspecialchars($data);
+		return $data;
+	}
+
 
   /**
    * @return array
@@ -11,5 +24,3 @@ class LoginController
   public static function login() {
     return [];
   }
-
-}
