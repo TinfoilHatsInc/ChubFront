21bd2f6ff43b0df977bc501dec8a9a7b6373be90
mskelo <matejskelo@gmail.com>
2017-12-19 14:59:08 +0100

    HTML form handling v0.0.0.0.1 lol

--- core/login/LoginController.php
+++ core/login/LoginController.php
@@ -3,11 +3,16 @@
 namespace core\login;
 
 $name = $password = $rememberMe = "";
+$emailError = $pwError = "";
 
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		$name = validateInput($_POST["uname"]);
-		$password = validateInput($_POST["pw"]);
-		$rememberMe = $_POST["checkbox"]);
+		if (filter_var($name, FILTER_VALIDATE_EMAIL)) {
+			$password = validateInput($_POST["pw"]);
+			$rememberMe = $_POST["checkbox"]);
+		} else {
+		  $emailError("Please enter a valid email address.");
+		}
 	}
 
 	public static function validateInput($data){
